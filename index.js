20b5de1e945e93909b37b7423f98bdbfe2cb8cd1
carolina794 <carolinaalejandravasque@gmail.com>
2025-05-30 22:19:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*alert ("hola mundo")
+document.write("hola mundo")
+console.log(2+5);
+var nombre = prompt("ingrese nombre")*/
+
+
+
+var nota = 7 ;
+console.log("nota:",nota);
+//condicion (si nota es mayor o igual a 5)
+if (nota>= 5){
+    console.log("¡estoy aprobada!");
+}
+
+
+
+
